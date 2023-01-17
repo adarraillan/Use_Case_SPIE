46e01bf7416e7e651d2c46bcc56c0039b11e614f
adarraillan <a.darraillan2@gmail.com>
2023-01-17 09:57:37 +0100

    Added mutate method to class_individual.py

--- class_Individual.py
+++ class_Individual.py
@@ -1,3 +1,7 @@
+import random
+
+matrice_type_puissance_par_demie_heure = {'LV':0.65, 'LL':1, 'SL':0.125, 'TV':0.1, 'FG_1':0.1, 'CE_1': 0.18, 'CG': 0.1, 
+                                                'FO': 1.6, 'PL': 1.2, 'FG_2': 0.3, 'CE_2': 0.25}
 
 
 class Individual:
@@ -11,6 +15,28 @@ class Individual:
         self.plannings = plannings
         self.day_consumption = day_consumption
 
+
+    def mutate(self, matrice_type_puissance_par_demie_heure):
+        house_index = random.randint(0, len(self.plannings))
+
+        planning_index = self.plannings[house_index]
+        machine_type = random.choice(list(planning_index.keys()))
+        
+        if machine_type == 'CG' || machine_type == 'FG_1' || machine_type == 'FG_2' || machine_type == 'CE_1' || machine_type == 'CE_2':
+            self.mutate_seq(machine_type, house_index)
+        else:
+            self.mutate_no_seq(machine_type, house_index)
+
+
     #a time unit is 30 minutes
-    def mutate_seq(self,HC : list(int),count_time_unit : int,machine_type : str, house_index : int):
+    def mutate_seq(self, machine_type : str, house_index : int):
         planning = self.plannings
+
+    def mutate_no_seq(self, machine_type : str, house_index : int):
+        planning = self.plannings
+  
+
+
+   
+
+
---
+++ codeSPIE/getMax.py
@@ -0,0 +1,10 @@
+#get the maximum value of a list
+def getMax(list):
+    max = list[0]
+    for i in list:
+        if i > max:
+            max = i
+    return max
+
+
+
